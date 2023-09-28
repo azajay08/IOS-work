b7066ffb391863b842fda5209e516f17abd4ddf4
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-09-28 12:59:20 +0300

    Initial Commit

---
+++ Seventeen Again/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seventeen Again
+//
+//  Created by Aaron Jones on 28.9.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seventeen Again/Seventeen_AgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seventeen_AgainApp.swift
+//  Seventeen Again
+//
+//  Created by Aaron Jones on 28.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct Seventeen_AgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
